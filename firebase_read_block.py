5538b94be2c8bca9e0e91e33613a56ad950e1bd9
Tyler Lugger <tlugger@n.io>
2017-06-08 17:56:09 +0000

    Created a very generic read block that pulls all data from a firebase database

---
+++ firebase_read_block.py
@@ -0,0 +1,20 @@
+from .firebase_base import FirebaseBase
+
+
+class FirebaseRead(FirebaseBase):
+
+    def process_signals(self, signals):
+        out_sigs = []
+        for sig in signals:
+            # The collection may be different for different signals,
+            # so we need to compute it inside the loop
+            collection = self._get_collection(sig)
+            try:
+                res = self._firebase.get(collection, None)
+            except:
+                self.logger.exception("Couldn't save signal")
+                continue
+            # Take the result of the save and enrich our incoming signal
+            # with the data
+            out_sigs.append(self.get_output_signal(res, sig))
+        self.notify_signals(out_sigs)
