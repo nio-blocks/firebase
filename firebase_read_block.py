a777a8820fb98d639de6b39fccd75c1803d61e70
Tyler Lugger <tlugger@n.io>
2017-06-09 16:00:53 +0000

    removed unneeded comments from read block

--- firebase_read_block.py
+++ firebase_read_block.py
@@ -6,15 +6,11 @@ class FirebaseRead(FirebaseBase):
     def process_signals(self, signals):
         out_sigs = []
         for sig in signals:
-            # The collection may be different for different signals,
-            # so we need to compute it inside the loop
             collection = self._get_collection(sig)
             try:
                 res = self._firebase.get(collection, None)
             except:
                 self.logger.exception("Couldn't get from collection")
                 continue
-            # Take the result of the save and enrich our incoming signal
-            # with the data
             out_sigs.append(self.get_output_signal(res, sig))
         self.notify_signals(out_sigs)
