9f161aad6ffecff2b08ca6fca5e3fe0034038eec
Tyler Lugger <tlugger@n.io>
2018-07-17 12:01:41 -0600

    Add auth refresh logic

--- firebase_base.py
+++ firebase_base.py
@@ -1,8 +1,10 @@
 import pyrebase
+from datetime import timedelta
 
 from nio import Block
-from nio.properties import ObjectProperty, StringProperty
+from nio.properties import ObjectProperty, StringProperty, IntProperty
 from nio.util.discovery import not_discoverable
+from nio.modules.scheduler.job import Job
 
 from .auth.property import FirebaseAuthProperty
 
@@ -10,13 +12,15 @@ from .auth.property import FirebaseAuthProperty
 @not_discoverable
 class FirebaseBase(Block):
 
-    config = ObjectProperty(FirebaseAuthProperty, title="Authentication")
+    config = ObjectProperty(FirebaseAuthProperty, title="Authentication", order=0)
     collection = StringProperty(title='Database Collection',
-                                default='[[FIREBASE_COLLECTION]]')
+                                default='[[FIREBASE_COLLECTION]]', order=3)
     userEmail = StringProperty(title='Authenticated User Email',
-                               default='[[USER_EMAIL]]')
+                               default='[[USER_EMAIL]]', order=1)
     userPassword = StringProperty(title='Authenticated User Password',
-                                  default='[[USER_PASSWORD]]')
+                                  default='[[USER_PASSWORD]]', order=2)
+    authRefresh = IntProperty(title='Auth Refresh Interval', default=3600,
+                              advanced=True, order=4)
 
     def __init__(self):
         super().__init__()
@@ -24,10 +28,32 @@ class FirebaseBase(Block):
         self.user = None
         self.db = None
         self.stream_start = False
+        self._refresh_job = None
 
     def configure(self, context):
         super().configure(context)
         self._create_firebase()
+
+    def start(self):
+        """ Starts the block
+
+        Begin the job to refresh the auth token every hour
+        """
+        super().start()
+        self._refresh_job = Job(self._refresh_auth,
+                                timedelta(seconds=self.authRefresh()),
+                                True)
+
+    def stop(self):
+        """ Stops the block
+
+        Cancel the token refresh job
+        """
+        self._refresh_job.cancel()
+        super().stop()
+
+    def _refresh_auth(self):
+        self.user = auth.refresh(self.user['refreshToken'])
 
     def _create_firebase(self):
         firebase = pyrebase.initialize_app(self.config().get_auth_object())
